3eff08cf644992f5bc1add7874b7f0cc9f81239f
Fasp96 <38566316+Fasp96@users.noreply.github.com>
2019-11-21 15:38:32 +0000

    Add image path to horses table

--- luckyhorse/database/migrations/2019_11_18_151622_create_horses_table.php
+++ luckyhorse/database/migrations/2019_11_18_151622_create_horses_table.php
@@ -17,10 +17,11 @@ class CreateHorsesTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('breed');
-            //$table->integer('number');
+            //$table->integer('race_number');
             $table->integer('age');
             $table->integer('num_races');
             $table->integer('num_victories');
+            $table->string('file_path');
             $table->timestamps();
         });
     }
