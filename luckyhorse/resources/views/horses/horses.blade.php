2daab1096fad1e9a8dc33332b3f0feb29572561b
André <andre_t3lo@hotmail.com>
2019-11-23 15:16:18 +0000

    horse page

--- luckyhorse/resources/views/horses/horses.blade.php
+++ luckyhorse/resources/views/horses/horses.blade.php
@@ -2,23 +2,36 @@
 
 @section('content')
 
+<style>
+#card-body{
+    background-color: lightblue;
+}
+</style>
+
+<h1 align="center">Horses</h1>
+@foreach($horses as $horse)
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Horses</div>
-
-                <div class="card-body">
-                    <ul>
-                        @foreach($horses as $horse) 
-                            <li>{{$horse->name}} - {{$horse->birth_date}}</li>                    
-                        @endforeach
-                    </ul>
+            <div class="card bg-dark text-white" >
+            <img class="card-img" src={{ asset('img/horse-galloping.jpg')}} alt="Card image" height="250" >
+             
+                <div class="card-img-overlay">
+                            <p>name: {{$horse->name}}</p>
+                            <p>breed: {{$horse->breed}}</p>
+                            <p>birth_date: {{$horse->birth_date}}</p>
+                            <p>gender: {{$horse->gender}}</p>
+                            <p>number of races: {{$horse->num_races}}</p>
+                            <p>number of victories: {{$horse->num_victories}}</p>
+                
                 </div>
+
             </div>
         </div>
     </div>
 </div>
 
+@endforeach
 
 @endsection
