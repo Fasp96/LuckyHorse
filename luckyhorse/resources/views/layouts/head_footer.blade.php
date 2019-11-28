566ab562787dd78ccbfc678c23a5496f298a3b31
Marcio-M <marcio.gmartins96@gmail.com>
2019-11-28 13:45:47 +0000

    Vue warning fix

--- luckyhorse/resources/views/layouts/head_footer.blade.php
+++ luckyhorse/resources/views/layouts/head_footer.blade.php
@@ -4,23 +4,26 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Luckyhorse</title>
-
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Luckyhorse</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
         
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        
 
     </head>
     <body>
+    <div id="app"></div>
         <div id="page-container">
             <div id="content-wrap">
                 <div id='logo_nav_bar'>
@@ -56,17 +59,18 @@
             </div>
             <footer>
                 <p>Posted by: André, Fábio e Márcio | Contacts | termos e condições |
-                <button onclick="topFunction()" id="myBtn" title="Go to top">Top
-                    <script>
-                        function topFunction() {
-                            document.body.scrollTop = 0;
-                            document.documentElement.scrollTop = 0;
-                        }
-                    </script>
-                </button>
+                <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
                 </p>
             </footer>
         </div>
-        
+
     </body>
+
+    <script>
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
+
 </html>
