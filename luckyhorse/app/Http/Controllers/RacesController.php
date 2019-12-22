9534ed0f7d5d93fea8a6d9c6ddb449ed5212f33c
André <andre_t3lo@hotmail.com>
2019-12-22 11:18:17 +0000

    fix redirection to race-page and some changes in that page

--- luckyhorse/app/Http/Controllers/RacesController.php
+++ luckyhorse/app/Http/Controllers/RacesController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Race;
+
+
+use App\Jockey;
+use App\Horse;
+use App\Result;
+
 use Auth;
 
 class RacesController extends Controller
@@ -13,6 +19,13 @@ class RacesController extends Controller
     public function index(){
         $current_user = Auth::user();
              $races = Race::all();
-             return view('races',compact('races'));   
+
+             $results = Result::all();
+
+            //    $jockey_names = Jockey::select('name')->where('id',Result::select('jockey_id'))->where('race_id',Race::select('id'))->get();
+            //   $horse_names = Horse::select('name')->where('id',Result::select('horse_id'))->where('race_id',Race::select('id'))->get();
+           
+
+             return view('races.races',compact('races'));   
     }
 }
