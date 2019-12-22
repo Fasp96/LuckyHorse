68528f2806def2890c5652f62d89e0632d5831ba
André <andre_t3lo@hotmail.com>
2019-12-22 17:23:15 +0000

    add some changes in tournaments-page

--- luckyhorse/resources/views/tournaments/tournaments.blade.php
+++ luckyhorse/resources/views/tournaments/tournaments.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
+<style>
+    img{
+        float:right;
+    }
+
+</style>
+
 <h1 align="center">Tournaments</h1>
 
 @foreach($tournaments as $tournament) 
@@ -12,8 +19,24 @@
                 <div class="card-header">Tournaments</div>
 
                 <div class="card-body">
-                        
-                            <p>name: {{$tournament->name}}</p>                  
+                    <img src="{{ $tournament->file_path}}" alt="tournament_img" style="width:40%;opacity:0.85;">
+                    name: {{$tournament->name}}<br>
+                    date: {{$tournament->date}}<br>
+                    description: {{$tournament->description}}<br>
+                    location: {{$tournament->location}}<br><br>
+                    
+
+                    @foreach($races as $race)
+                    <?php
+                        if($tournament->id == $race->tournament_id){
+                            echo "race: {$race->name} <br>";
+                        }
+                        else{
+                            echo "no races to this tournament <br>";
+                        }
+                    
+                    ?>
+                    @endforeach
                         
                 </div>
             </div>
