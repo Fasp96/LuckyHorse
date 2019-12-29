4b4d9840200ab90869560a35954e1c923a37ff81
Marcio-M <marcio.gmartins96@gmail.com>
2019-12-29 13:38:23 +0000

    news table migration fix

--- luckyhorse/database/migrations/2019_12_27_111106_create_news_table.php
+++ luckyhorse/database/migrations/2019_12_27_111106_create_news_table.php
@@ -18,7 +18,7 @@ class CreateNewsTable extends Migration
             $table->string('title');
             $table->text('description');
             $table->string('file_path');
-            $table->string('minute_info');
+            $table->string('minute_info')->nullable();
 
             $table->timestamps();
         });
