8cbd06ac947bd9b927e3b7e3713a3e33eac37637
André <andre_t3lo@hotmail.com>
2019-12-29 18:06:43 +0000

    button in race page

--- luckyhorse/resources/views/races/races.blade.php
+++ luckyhorse/resources/views/races/races.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.head_footer')
 
 @section('content')
+
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 <style>
     img {
     float: right;
@@ -28,12 +32,36 @@
         color: red;
         font-weight:bold; 
     }
+    h3{
+        color: white;
+         
+    }
+
+    .button {
+        background-color:#323232;
+        border: none;
+        padding: 15px 30px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        width: 99%;
+        height: 50px;
+        
+    }
+
+    .button:hover {
+        background-color: orange;
+        color: white;
+    }
 </style>
 
+</head>
+
+<body>
 <h1 align="center">Races</h1>
-
-
-
 
 @foreach($races as $race)
 
@@ -42,9 +70,15 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                   <div class="card-header"> <h2>{{$race->name}}</h2></div>  
-             <!--   <button onclick="myFunction()">{{$race->name}}</button> -->
+                <!--   <div class="card-header"> <h2>{{$race->name}}</h2></div>  -->
+
+                
                     <div id="body-card">
+
+                    <div class="button-container">
+                        <button class="button" id="{{$race->id}}"><h3>{{$race->name}}</h3></button> 
+                    </div>
+
                         <div class="card-body">
                             <img src="{{ $race->file_path}}" alt="race_img" style="width:40%;opacity:0.85;">
                             
@@ -64,151 +98,6 @@
                             ?>
                             @endforeach
                             
-
-                            <!--   -------------devolve os cavalos de cada corrida----------------    -->
-                            <h3>Horses in this race: </h3>
-                            
-                            <ul>  
-
-                            @foreach($results as $result)
-                            <?php
-                            if($result->race_id == $race->id){
-                                
-                                 //   echo "<li>{$race->time}</li>"
-                            ?>
-
-                            @foreach($horses as $horse)
-                            
-                            <?php
-                                if($result->horse_id == $horse->id){
-                                    echo "<li> {$horse->name}</li>";   
-                                }  
-                            ?>
-                            @endforeach
-                            
-                            
-                            
-                            <?php
-                            }else{
-                                
-                            }
-                            ?>
-                            
-
-                            @endforeach
-                            
-                            <!-- -------------------  devolve os jockeys de cada corrida -------------------------- -->
-                            </ul>
-
-                            <h3>Jockeys in this race: </h3>
-                                
-                            <ul>  
-
-                            @foreach($results as $result)
-                            <?php
-                            if($result->race_id == $race->id){
-                                
-                                 //   echo "<li>{$race->time}</li>"
-                            ?>
- 
-                            @foreach($jockeys as $jockey)
-                            
-                            <?php
-                                if($result->jockey_id == $jockey->id){
-                                    echo "<li> {$jockey->name} </li>";   
-                                }  
-                            ?>
-                            @endforeach
-                            
-                            
-                            
-                            <?php
-                            }else{
-                               // echo "no results";
-                            }
-                            ?>
-                            
-
-                            @endforeach
-
-                            </ul>
-
-                            <!-- -------------------  devolve os resultados de cada corrida -------------------------- -->
-                            <h3>Results: </h3>
-
-                            <table style="width:60%">
-                                <tr>
-                                    <th>Horse</th>
-                                    <th>Jockey</th> 
-                                    <th>Time</th>
-                                </tr>
-
-                            @foreach($results as $result)
-                            <?php
-                            if($result->race_id == $race->id){
-                            
-                            if(($result->time) == null){
-                                echo "<tr><td colspan='3'> This race don't have results yet </td></tr>";
-                            }
-                                
-                            //se a corrida já tiver resultados
-                            if(($result->time) != null){ 
-                            ?>
-
-                               
-                            @foreach($horses as $horse)
-                            
-                                <?php
-                                    if($result->horse_id == $horse->id){
-                                        echo "  <tr><td>{$horse->name}</td>";   
-                                    }  
-                                ?>
-                            @endforeach 
-
-                            @foreach($jockeys as $jockey)
-                            
-                            <?php
-                                if($result->jockey_id == $jockey->id){
-                                    echo "<td>{$jockey->name}</td> ";
-                                
-                                    
-                                    
-                                }  
-                            ?>
-                            @endforeach
-                            
-
-                                            
-                            <?php
-                            
-                                    echo "<td>{$result->time}</td> ";
-                                    echo "</tr>";
-                                    }
-
-                            
-                            }else{
-                               // echo "no results";
-                            }
-                            
-                            ?>
-                            @endforeach
-
-                            </table>
-                            
-                            <!-- --------------------- devolve o vencedor de cada corrida ------------------------ -->
-                          <br>
-
-                            
-                            @foreach($winners as $winner)
-                                @if($winner[0]->race_id==$race->id)
-                                    @if($winner[0]->time != null)
-                                        <h3>Winner: </h3>
-                                        <h4 align="center">    {{$winner[0]->name}} with a best time: {{$winner[0]->time}}</h4>
-                                    @endif
-                                @endif
-                            @endforeach
-    
-
                     </div>
                 </div>
             </div>
@@ -222,6 +111,16 @@
 
 @endforeach
 
+<script>
+        $("body").on( "click", ".button-container button",function(){
+        //var y = $(this).text();
+        var x = $(this).attr('id');
+        
+        location.replace("http://localhost:8000/races/" + x);   
+    });
 
+</script>
+
+</body>
 @endsection
 
---
+++ luckyhorse/resources/views/races/races_info.blade.php
@@ -0,0 +1,212 @@
+@extends('layouts.head_footer')
+
+@section('content')
+<style>
+    img {
+    float: right;
+    }
+
+    table, td {
+        border: 2px solid black;
+        border-collapse: collapse;
+        background-color: #FFE4C4;
+}
+
+    th{
+        color: white;
+        border: 2px solid black;
+        border-collapse: collapse;
+        background-color: grey;
+    }
+
+    h4{
+        color: green;
+        font-weight:bold;
+    }
+
+    h2{
+        color: red;
+        font-weight:bold; 
+    }
+</style>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                   <div class="card-header"> <h2>{{$races->name}}</h2></div>  
+             <!--   <button onclick="myFunction()">{{$races->name}}</button> -->
+                    <div id="body-card">
+                        <div class="card-body">
+                            <img src="{{ $races->file_path}}" alt="race_img" style="width:40%;opacity:0.85;">
+                            
+                            
+                            Date: {{$races->date}}<br>
+                            Description: {{$races->description}}<br>
+                            Local: {{$races->location}}<br>
+
+                            @foreach($tournaments as $tournament)
+
+                            <?php 
+                            if($races->tournament_id == $tournament->id){ 
+                                echo "Tournament: {$tournament->name}<br><br>";  
+                            }else{
+                                echo "no tournament to this race<br><br>";
+                            }
+                            ?>
+                            @endforeach
+                            
+
+                            <!--   -------------devolve os cavalos de cada corrida----------------    -->
+                            <h3>Horses in this race: </h3>
+                            
+                            <ul>  
+
+                            @foreach($results as $result)
+                            <?php
+                            if($result->race_id == $races->id){
+                                
+                                 //   echo "<li>{$race->time}</li>"
+                            ?>
+
+                            @foreach($horses as $horse)
+                            
+                            <?php
+                                if($result->horse_id == $horse->id){
+                                    echo "<li> {$horse->name}</li>";   
+                                }  
+                            ?>
+                            @endforeach
+                            
+                            
+                            
+                            <?php
+                            }else{
+                                
+                            }
+                            ?>
+                            
+
+                            @endforeach
+                            
+                            <!-- -------------------  devolve os jockeys de cada corrida -------------------------- -->
+                            </ul>
+
+                            <h3>Jockeys in this race: </h3>
+                                
+                            <ul>  
+
+                            @foreach($results as $result)
+                            <?php
+                            if($result->race_id == $races->id){
+                                
+                                 //   echo "<li>{$race->time}</li>"
+                            ?>
+ 
+                            @foreach($jockeys as $jockey)
+                            
+                            <?php
+                                if($result->jockey_id == $jockey->id){
+                                    echo "<li> {$jockey->name} </li>";   
+                                }  
+                            ?>
+                            @endforeach
+                            
+                            
+                            
+                            <?php
+                            }else{
+                               // echo "no results";
+                            }
+                            ?>
+                            
+
+                            @endforeach
+
+                            </ul>
+
+                            <!-- -------------------  devolve os resultados de cada corrida -------------------------- -->
+                            <h3>Results: </h3>
+
+                            <table style="width:60%">
+                                <tr>
+                                    <th>Horse</th>
+                                    <th>Jockey</th> 
+                                    <th>Time</th>
+                                </tr>
+
+                            @foreach($results as $result)
+                            <?php
+                            if($result->race_id == $races->id){
+                            
+                            if(($result->time) == null){
+                                echo "<tr><td colspan='3'> This race don't have results yet </td></tr>";
+                            }
+                                
+                            //se a corrida já tiver resultados
+                            if(($result->time) != null){ 
+                            ?>
+
+                               
+                            @foreach($horses as $horse)
+                            
+                                <?php
+                                    if($result->horse_id == $horse->id){
+                                        echo "  <tr><td>{$horse->name}</td>";   
+                                    }  
+                                ?>
+                            @endforeach 
+
+                            @foreach($jockeys as $jockey)
+                            
+                            <?php
+                                if($result->jockey_id == $jockey->id){
+                                    echo "<td>{$jockey->name}</td> ";
+                                
+                                    
+                                    
+                                }  
+                            ?>
+                            @endforeach
+                            
+
+                                            
+                            <?php
+                            
+                                    echo "<td>{$result->time}</td> ";
+                                    echo "</tr>";
+                                    }
+
+                            
+                            }else{
+                               // echo "no results";
+                            }
+                            
+                            ?>
+                            @endforeach
+
+                            </table>
+                            
+                            <!-- --------------------- devolve o vencedor de cada corrida ------------------------ -->
+                          <br>
+
+                            
+                            @foreach($winners as $winner)
+                                @if($winner[0]->race_id==$races->id)
+                                    @if($winner[0]->time != null)
+                                        <h3>Winner: </h3>
+                                        <h4 align="center">    {{$winner[0]->name}} with a best time: {{$winner[0]->time}}</h4>
+                                    @endif
+                                @endif
+                            @endforeach
+    
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+
+@endsection
