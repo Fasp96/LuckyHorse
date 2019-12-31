61453976f25d63b03bd8c6827a5814bf2e99754b
Marcio-M <marcio.gmartins96@gmail.com>
2019-12-31 17:21:19 +0000

    bets-page

--- luckyhorse/resources/views/bets/bets.blade.php
+++ luckyhorse/resources/views/bets/bets.blade.php
@@ -3,121 +3,58 @@
 @section('content')
 
 <style>
-
-    /*Last Results Tables*/
-    #last_results_tables{
-        display: flex;
-        justify-content: space-evenly;
-        color: white;
+    h2{
+        font-weight:bold; 
     }
-    #last_results_tables [class="result_table"]{
-        padding: 1px;
+    h1{
+        font-weight:bold; 
     }
-    #last_results_tables [class="tab_title"]{
-        background-color: #fa8b1b;
-        border: 1px solid black;
-    }
-    #last_results_tables [class="tab_tags"]{
-        background-color: #333;
-        border: 1px solid black;
-        font-weight: bold;
-        padding: 5px;
-    }
-    #last_results_tables [class="tab_result"]{
-        background-color: grey;
-        border: 1px solid black;
-    }
-    #last_results_tables [class="tab_position"]{
-        background-color: grey;
-        font-weight: bold;
-    }
-    caption {
-        caption-side: top;
-        color: green;
-        text-align: center;
-        padding: 0px;
-        font-weight: bold;
-        width: 600px;
-    }
-    table {
-        text-align: center;
-    }
-    td{
-        padding: 3px;
-    }
-    a{
-        color: white;
-    }
-
-    
 </style>
 
+<h1 align="center">Bets</h1>
 
+@foreach($tournament_bets as $tournament_bet) 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"> <h2>Bet ID: {{$tournament_bet->bet_id}}</h2></div>
+                <div class="card-body">
 
-<div id="last_results_tables">
-    <table class="result_table">
-        <caption class="tab_title">
-            <div>Bets</div>
-        </caption>
+                    Tournament Name: {{$tournament_bet->tournament_name}}<br>
+                    Date: {{$tournament_bet->date}}<br>
+                    Horse Name: {{$tournament_bet->horse_name}}<br>
 
-        <tr class="tab_tags">
-        <!--    <td></td>  -->
-            <td>Race</td>
-            <td class="table_center_element">Tournament</td>
-            <td>Horse</td>
-            <td>Value</td>
-        </tr>
+                    Value: {{$tournament_bet->value}}<br>                
 
-        @foreach ($races as $race)
-            <?php
-                if(date('Y-m-d') < $race->date){
-                    //echo "{$race->name}";
-                    ?>
-                    
-
-
-                            @foreach($bets as $bet)
-                                @if($bet->race_id==$race->id)
-                                    
-
-                                        <tr class="tab_result">
-                                      <!--     <td class="tab_position">{{$race->id}}</td> -->
-                                            <td><a href="/races/{{$race->id}}">{{$race->name}}</a></td>
-
-                                            @foreach($tournaments as $tournament)
-                                               @if( $bet->tournament_id == $tournament->id )  
-                                                   <td><a href="/tournaments/{{$bet->tournament_id}}"> {{$tournament->name}} </a></td>
-                                               @endif 
-                                            @endforeach
-
-
-                                            @foreach($horses as $horse)
-                                                @if( $bet->horse_id == $horse->id ) 
-                                                    <td><a href="/horses/{{$bet->horse_id}}">{{$horse->name}}</a></td>
-                                                @endif 
-                                            @endforeach
-
-                                            <td>{{$bet->value}}</td>
-                                            
-                                        </tr>
-                                
-                                @endif
-                            @endforeach
-                        
-                         </div> 
                        
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
+
+@foreach($race_bets as $race_bet) 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"> <h2>Bet ID: {{$race_bet->bet_id}}</h2></div>
+                <div class="card-body">
+
+                    Race Name: {{$race_bet->race_name}}<br>
+                    Date: {{$race_bet->date}}<br>
+                    Horse Name: {{$race_bet->horse_name}}<br>
                     
-            
-               
-            <?php  }else{
-                        //date('Y-m-d') > $race->date
-                }
-                ?>
-          
-        @endforeach
-
-    </table>
-
+                    Value: {{$race_bet->value}}<br>                
+                       
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
 
 
 @endsection
