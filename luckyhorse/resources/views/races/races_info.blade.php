f0b5802382b30d30369e320d6954f082f4968c67
Marcio-M <marcio.gmartins96@gmail.com>
2020-01-02 21:36:49 +0000

    races_info blade fix

--- luckyhorse/resources/views/races/races_info.blade.php
+++ luckyhorse/resources/views/races/races_info.blade.php
@@ -51,113 +51,97 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                   <div class="card-header"> <h2>{{$results[0]->name}}</h2></div>
-                    <div id="body-card">
-                        <div class="card-body">
-                            <img src="{{ $results[0]->file_path}}" alt="race_img" style="width:50%;opacity:0.85;">
-                            
-                            Date: {{date('d-m-Y', strtotime($results[0]->date))}}<br>
-                            Time: {{date('H:i:s', strtotime($results[0]->date))}}<br>
-                            Description: {{$results[0]->description}}<br>
-                            Local: {{$results[0]->location}}<br>
-                            @isset($results[0]->tournament_name)
-                                Tournament: {{$results[0]->tournament_name}}<br>
-                            @endif
-                            <br>
-                            <!--   -------------devolve os cavalos de cada corrida----------------    -->
-                            <h3>Horses in this race: </h3>
-                            <ul>  
-                            @foreach($results as $result)
-                                <li><a href='/horses/{{$result->horse_id}}'>{{$result->horse_name}}</a></li>
-                            @endforeach
-                            </ul>
-                            
-                            <!-- -------------------  devolve os jockeys de cada corrida -------------------------- -->
-                            <h3>Jockeys in this race: </h3>
-                            <ul>  
-                            @foreach($results as $result)
-                                <li><a href='/jockeys/{{$result->jockey_id}}'>{{$result->jockey_name}}</a></li>
-                            @endforeach
-                            </ul>
+                <div class="card-header"> <h2>{{$results[0]->name}}</h2></div>
+                <div id="body-card">
+                    <div class="card-body">
+                        <img src="{{ $results[0]->file_path}}" alt="race_img" style="width:50%;opacity:0.85;">
+                        
+                        Date: {{date('d-m-Y', strtotime($results[0]->date))}}<br>
+                        Time: {{date('H:i:s', strtotime($results[0]->date))}}<br>
+                        Description: {{$results[0]->description}}<br>
+                        Local: {{$results[0]->location}}<br>
+                        @isset($results[0]->tournament_name)
+                            Tournament: {{$results[0]->tournament_name}}<br>
+                        @endif
+                        <br>
+                        <!--   -------------devolve os cavalos de cada corrida----------------    -->
+                        <h3>Horses in this race: </h3>
+                        <ul>  
+                        @foreach($results as $result)
+                            <li><a href='/horses/{{$result->horse_id}}'>{{$result->horse_name}}</a></li>
+                        @endforeach
+                        </ul>
+                        
+                        <!-- -------------------  devolve os jockeys de cada corrida -------------------------- -->
+                        <h3>Jockeys in this race: </h3>
+                        <ul>  
+                        @foreach($results as $result)
+                            <li><a href='/jockeys/{{$result->jockey_id}}'>{{$result->jockey_name}}</a></li>
+                        @endforeach
+                        </ul>
 
-                            <!-- -------------------  devolve os resultados de cada corrida -------------------------- -->
-                            <h3>Results: </h3>
-                            <table style="width:60%">
-                                <tr>
-                                    <th>Win Prob</th>
-                                    <th>Horse</th>
-                                    <th>Jockey</th> 
-                                    <th>Time</th>
-                                    @auth
-                                        @if(Auth::user()->role=='admin')
-                                       
-                                            <th> </th>
-                                        @endif
-                                    @endauth
-
-                                 </tr>
-                                @foreach($results as $result)
-                                
-                                   
-                                
-                                    <tr>
-                                        @foreach($scores as $score)
-                                            @if($score->horse_id==$result->horse_id)
-                                                <td>{{($score->wr/$win_total)}}</td>
-                                            @endif
-                                        @endforeach
-                                        <td>{{$result->horse_name}}</td>
-                                        <td>{{$result->jockey_name}}</td>
-                                        @isset($result->time)
-                                            <td>{{$result->time}}</td>
-                                        @else
-                                            <td>00:00:00</td>
-                                        @endif 
-
-                                        @auth
-                                        @if(Auth::user()->role=='admin')
-                                       
-                                        <td>
-                                       
-
-                                    
-                                    
-
-                                            <div class="edit_button">
-                                                <a href="/edit_results={{$result->id}}">Edit Result</a>
-                                            </div>
-                                        </td>
-                                        @endif
-                                        @endauth
-                                    </tr>
-                                @endforeach
-                            </table>
-                            <br>
-                            <!-- --------------------- devolve o vencedor de cada corrida ------------------------ -->
-                            @isset($winner->time)
-                                <h4 align="center">{{$winner->horse_name}} and {{$winner->jockey_name}} with a best time: 
-                                {{$winner->time}}</h4>
-                            @endif
-
-                            <div>
+                        <!-- -------------------  devolve os resultados de cada corrida -------------------------- -->
+                        <h3>Results: </h3>
+                        <table style="width:60%">
+                            <tr>
+                                <th>Win Prob</th>
+                                <th>Horse</th>
+                                <th>Jockey</th> 
+                                <th>Time</th>
                                 @auth
                                     @if(Auth::user()->role=='admin')
-                                        <div class="edit_button">
-                                            <a href="/edit_race={{$results[0]->race_id}}">Edit Race</a>
-                                        </div>
-                                        
-                                        
-                                        
-                                    @endif  
-                                    @if(!isset($winner->time))
-                                    <div class="edit_button bet_button">
-                                        <a href="/add_bet_race={{$results[0]->race_id}}">Bet</a>
-                                            
-                                    </div>
+                                        <th> </th>
                                     @endif
                                 @endauth
-                            </div>
-                            <br>
+                                </tr>
+                            @foreach($results as $result)
+                                <tr>
+                                    @foreach($scores as $score)
+                                        @if($score->horse_id==$result->horse_id)
+                                            <td>{{round(($score->wr/$win_total),2)}}</td>
+                                        @endif
+                                    @endforeach
+                                    <td>{{$result->horse_name}}</td>
+                                    <td>{{$result->jockey_name}}</td>
+                                    @isset($result->time)
+                                        <td>{{$result->time}}</td>
+                                    @else
+                                        <td>00:00:00</td>
+                                    @endif 
+                                    @auth
+                                        @if(Auth::user()->role=='admin')
+                                            <td>
+                                                <div class="edit_button">
+                                                    <a href="/edit_results={{$result->id}}">Edit Result</a>
+                                                </div>
+                                            </td>
+                                        @endif
+                                    @endauth
+                                </tr>
+                            @endforeach
+                        </table>
+                        <br>
+
+                        <!-- --------------------- devolve o vencedor de cada corrida ------------------------ -->
+                        @isset($winner->time)
+                            <h4 align="center">{{$winner->horse_name}} and {{$winner->jockey_name}} with a best time: 
+                            {{$winner->time}}</h4>
+                        @endif
+                        <div>
+                            @auth
+                                @if(Auth::user()->role=='admin')
+                                    <div class="edit_button">
+                                        <a href="/edit_race={{$results[0]->race_id}}">Edit Race</a>
+                                    </div>
+                                @endif  
+                                @if(!isset($winner->time))
+                                    <div class="edit_button bet_button">
+                                        <a href="/add_bet_race={{$results[0]->race_id}}">Bet</a>
+                                    </div>
+                                @endif
+                            @endauth
+                        </div>
+                        <br>
                     </div>
                 </div>
             </div>
