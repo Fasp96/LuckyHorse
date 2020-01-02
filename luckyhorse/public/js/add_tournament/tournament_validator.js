a73a4fe384641c3afa4fbce1d16fdaf18d8d8e2c
André <andre_t3lo@hotmail.com>
2020-01-02 16:05:55 +0000

    add forms fix

--- luckyhorse/public/js/add_tournament/tournament_validator.js
+++ luckyhorse/public/js/add_tournament/tournament_validator.js
@@ -59,7 +59,7 @@ function validate_input(clicked = false){
     //if everything is filled and validated it will remove the existing button and add a button inside the <form> to use the post method
     if(valid.reduce(and) && not_empty.reduce(and)){
         $("#add_tournament_btn").remove();
-        $("#tournament_photo").after("<br><br><button type=\"submit\" class=\"btn btn-primary\">Add Tournament</button>");
+        $("#form_end").after("<button id='add_tournament_btn' type=\"submit\" class=\"btn btn-primary\">Add Tournament</button>");   
     }
 }
 
--- luckyhorse/resources/views/jockeys/add_jockey.blade.php
+++ luckyhorse/resources/views/jockeys/add_jockey.blade.php
@@ -50,6 +50,7 @@
                         Jockey Photo <br><br>
                         <input id="jockey_photo" type="file" name="jockey_photo" accept="image/*" onchange="validate_input()"><br>
                         <br>
+                        <div id="form_end"></div>
                     </form>
                     <button id="add_jockey_btn" class="btn btn-primary">Add Jockey</button>
 
