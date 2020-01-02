c3320f23466bee99dd07961ef39bb6ba66965893
Marcio-M <marcio.gmartins96@gmail.com>
2020-01-02 11:43:21 +0000

    Logout

--- luckyhorse/resources/views/auth/login.blade.php
+++ luckyhorse/resources/views/auth/login.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.head_footer')
 
+<style>
+    h2{
+    color: #333;
+  }
+</style>
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+                <div class="card-header"><h2>{{ __('Login') }}</h2></div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
